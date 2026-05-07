29675ab28d1a5378a9e888bf44ab15954eec290b
robtinkers <74528586+robtinkers@users.noreply.github.com>
2026-05-07 16:14:41 +0100

    Update client_ish.py

--- http/client_ish.py
+++ http/client_ish.py
@@ -631,7 +631,7 @@ class HTTPConnection:
 
         self._method = method
         self._url = url
-        self._putheaderparts(False, self._method, b" ", self._url, b" HTTP/1.1\r\n")
+        self._putheaderparts(False, method, b" ", url, b" HTTP/1.1\r\n")
 
         if not skip_host:
             host = self.host.encode(_ENCODE_HEAD)
