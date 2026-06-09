6543e216b0abc9d5c8b04f87be193e1d3d1ce7e7
robtinkers <74528586+robtinkers@users.noreply.github.com>
2026-06-09 12:55:52 +0100

    Update latin1.py

--- latin1.py
+++ latin1.py
@@ -1,55 +1,37 @@
 
 @micropython.viper
-def _is_visible_latin1(buf:ptr8, buflen:int) -> int:
+def _latin1_to_utf8(buf: ptr8, length: int, dst: ptr8) -> int:
+    write = int(dst) != 0
+    dstlen = 0
     i = 0
-    while (i < buflen):
+    while i < length:
         b = buf[i]
+        if b < 128:
+            if write:
+                dst[dstlen] = b
+            dstlen += 1
+        elif b < 160:
+            return -1
+        else:
+            if write:
+                dst[dstlen+0] = 0xC0 | (b >> 6)
+                dst[dstlen+1] = 0x80 | (b & 0x3F)
+            dstlen += 2
         i += 1
-        if b < 32 or (127 <= b < 160):
-            return 0
-    return 1
+    return dstlen
 
-def latin1_to_utf8(s):
-    @micropython.viper
-    def latin1_to_utf8_helper(src: ptr8, srclen: int, dst: ptr8) -> int:
-        write = int(dst) != 0
-        dstlen = 0
-        is7bit = 1
-        
-        i = 0
-        while (i < srclen):
-            b = src[i]
-            i += 1
-            
-            if b < 128:
-                if write:
-                    dst[dstlen] = b
-                dstlen += 1
-            elif b < 192:
-                is7bit = 0
-                if write:
-                    dst[dstlen+0] = 0xC2
-                    dst[dstlen+1] = b
-                dstlen += 2
-            else:
-                is7bit = 0
-                if write:
-                    dst[dstlen+0] = 0xC3
-                    dst[dstlen+1] = b - 64
-                dstlen += 2
-        
-        return 0 if is7bit else dstlen
-    
-    len_s = len(s)
-    if len_s == 0:
-        return b''
-    utf8len = latin1_to_utf8_helper(s, len_s, 0)
-    if utf8len == 0:
-        return s.decode()
-    utf8buf = bytearray(utf8len)
-    latin1_to_utf8_helper(s, len_s, utf8buf)
-    return utf8buf.decode()
+def _decode_latin1(buf):
+    buflen = len(buf)
+    if buflen == 0:
+        return ''
+    utf8len = _latin1_to_utf8(buf, buflen, 0)
+    if utf8len < 0:
+        raise UnicodeError
+    if utf8len == buflen and hasattr(buf, "decode"):
+        return buf.decode()
+    utf8dst = bytearray(utf8len)
+    _latin1_to_utf8(buf, buflen, utf8dst)
+    return utf8dst.decode()
 
-print(latin1_to_utf8(b'hello!'))
-print(latin1_to_utf8(b'\xDF\xCA\x54\xC5'))
-
+print(_decode_latin1(b'hello!'))
+print(_decode_latin1(b'\xDF\xCA\x54\xC5'))
