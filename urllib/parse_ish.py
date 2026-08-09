57c368480a74942b624d83aeb1c386f0d544a623
robtinkers <74528586+robtinkers@users.noreply.github.com>
2026-08-09 18:27:42 +0100

    Update parse_ish.py

--- urllib/parse_ish.py
+++ urllib/parse_ish.py
@@ -15,47 +15,46 @@ _HEX_DIGITS = b"0123456789ABCDEF"
 
 _COMPILED_EMPTY = (
     b"\x00\x00\x00\x69"
-    b"\x00\x60\xff\x03"  # 0-9, -, .
-    b"\xfe\xff\xff\x87"  # A-Z, _
-    b"\xfe\xff\xff\x07"  # a-z
+    b"\x00\x60\xff\x03"  # 0-9 - .
+    b"\xfe\xff\xff\x87"  # A-Z _
+    b"\xfe\xff\xff\x47"  # a-z ~
 )
 _COMPILED_SLASH = (
     b"\x00\x00\x00\x69"
-    b"\x00\xe0\xff\x03"  # slash
+    b"\x00\xe0\xff\x03"  # /
     b"\xfe\xff\xff\x87"
-    b"\xfe\xff\xff\x07"
+    b"\xfe\xff\xff\x47"
 )
 _COMPILED_PLUS = (
     b"\x01\x00\x00\x69"  # plus mode
     b"\x00\x60\xff\x03"
     b"\xfe\xff\xff\x87"
-    b"\xfe\xff\xff\x07"
+    b"\xfe\xff\xff\x47"
 )
 
-class _compiled_blob(bytearray):
+class _compiled_safe(bytearray):
     def __init__(self):
         super().__init__(_COMPILED_EMPTY)
 
 def compile_safe(safe, flags=0):
     if flags not in (0, 1):
         raise ValueError("flags must be 0 (quote) or 1 (quote_plus)")
-    safeblob = _compiled_blob()
-    safeblob[0] = flags
+    compiled = _compiled_safe()
+    compiled[0] |= flags
     for c in safe:
         if isinstance(c, str):
             c = ord(c)
         if 32 <= c <= 127:
-            safeblob[c >> 3] |= 1 << (c & 7)
-    return safeblob
+            compiled[c >> 3] |= 1 << (c & 7)
+    return compiled
 
 @micropython.viper
-def _quote_helper(src: ptr8, srclen: int, safeblob_obj: object, res: ptr8) -> int:
-    safeblob = ptr8(safeblob_obj)
-    write = int(res) != 0
-    flags = safeblob[0]
-
-    if (flags != 0 and flags != 1) or safeblob[3] != 0x69:
-        return -999
+def _quote_helper(src: ptr8, srclen: int, safe_obj: object, res: ptr8) -> int:
+    safe_ptr = ptr8(safe_obj)
+    if len(safe_obj) != 16 or safe_ptr[3] != 0x69:
+        raise ValueError("invalid compiled safe")
+    plusmode = safe_ptr[0] & 1
+    mutable = int(res) != 0
 
     hex_digits = ptr8(_HEX_DIGITS)
     modified = 0
@@ -66,25 +65,25 @@ def _quote_helper(src: ptr8, srclen: int, safeblob_obj: object, res: ptr8) -> in
         b = src[i]
         i += 1
 
-        if b == 32 and flags:
+        if b == 32 and plusmode:
             modified = 1
-            if write:
+            if mutable:
                 res[reslen] = 43
             reslen += 1
             continue
 
         if 32 <= b and b < 128:
-            is_safe = (safeblob[b >> 3] >> (b & 7)) & 1
+            is_safe = (safe_ptr[b >> 3] >> (b & 7)) & 1
         else:
             is_safe = 0
 
         if is_safe:
-            if write:
+            if mutable:
                 res[reslen] = b
             reslen += 1
         else:
             modified = 1
-            if write:
+            if mutable:
                 res[reslen] = 37
                 res[reslen + 1] = hex_digits[b >> 4]
                 res[reslen + 2] = hex_digits[b & 15]
@@ -94,7 +93,7 @@ def _quote_helper(src: ptr8, srclen: int, safeblob_obj: object, res: ptr8) -> in
 
 def _quote(src, safe, flags):
     srclen = len(src)
-    compiled = isinstance(safe, _compiled_blob)
+    compiled = isinstance(safe, _compiled_safe)
 
     if compiled:
         if len(safe) != 16 or safe[0] != flags:
@@ -125,28 +124,26 @@ def quote(s, safe="/"):
         res = _quote(memoryview(s), safe, 0)
         if res is None:
             return s
-        return res.decode()
-    else:
+    elif isinstance(s, (bytes, bytearray)):
         res = _quote(s, safe, 0)
         if res is None:
             res = s
-            if isinstance(res, memoryview):
-                res = bytes(res)
-        return res.decode()
+    else:
+        raise TypeError()
+    return res.decode()
 
 def quote_plus(s, safe=""):
     if isinstance(s, str):
         res = _quote(memoryview(s), safe, 1)
         if res is None:
             return s
-        return res.decode()
-    else:
+    elif isinstance(s, (bytes, bytearray)):
         res = _quote(s, safe, 1)
         if res is None:
             res = s
-            if isinstance(res, memoryview):
-                res = bytes(res)
-        return res.decode()
+    else:
+        raise TypeError()
+    return res.decode()
 
 def quote_from_bytes(bs, safe="/"):
     if not isinstance(bs, (bytes, bytearray)):
@@ -159,14 +156,17 @@ def quote_to_bytes(s, safe="/"):
         res = _quote(memoryview(s), safe, 0)
         if res is None:
             return s.encode()
-        return res
-    else:
+    elif isinstance(s, bytes):
         res = _quote(s, safe, 0)
         if res is None:
-            res = s
-            if isinstance(res, memoryview):
-                res = bytes(res)
-        return res
+            return s
+    elif isinstance(s, bytearray):
+        res = _quote(s, safe, 0)
+        if res is None:
+            return bytes(s)
+    else:
+        raise TypeError()
+    return res
 
 # Extension
 def quote_plus_to_bytes(s, safe=""):
@@ -174,14 +174,17 @@ def quote_plus_to_bytes(s, safe=""):
         res = _quote(memoryview(s), safe, 1)
         if res is None:
             return s.encode()
-        return res
-    else:
+    elif isinstance(s, bytes):
         res = _quote(s, safe, 1)
         if res is None:
-            res = s
-            if isinstance(res, memoryview):
-                res = bytes(res)
-        return res
+            return s
+    elif isinstance(s, bytearray):
+        res = _quote(s, safe, 1)
+        if res is None:
+            return bytes(s)
+    else:
+        raise TypeError()
+    return res
 
 @micropython.viper
 def _unquote_helper(src: ptr8, start: int, end: int, res: ptr8) -> int:
@@ -190,7 +193,7 @@ def _unquote_helper(src: ptr8, start: int, end: int, res: ptr8) -> int:
         plusmode = 1
     else:
         plusmode = 0
-    write = int(res) != 0
+    mutable = int(res) != 0
 
     modified = 0
     reslen = 0
@@ -228,7 +231,7 @@ def _unquote_helper(src: ptr8, start: int, end: int, res: ptr8) -> int:
             modified = 1
             b = 32 # space
 
-        if write:
+        if mutable:
             res[reslen] = b
         reslen += 1
 
@@ -271,21 +274,32 @@ def unquote_plus(s):
         s = memoryview(s)
     return _unquote(s, 0, None, True).decode()
 
-# Extension
 def unquote_to_bytes(s):
     if isinstance(s, str):
         s = memoryview(s)
-    return _unquote(s, 0, None, False)
+    res = _unquote(s, 0, None, False)
+    if res is s and not isinstance(res, bytes):
+        res = bytes(res)
+    return res
 
 # Extension
 def unquote_plus_to_bytes(s):
     if isinstance(s, str):
         s = memoryview(s)
-    return _unquote(s, 0, None, True)
+    res = _unquote(s, 0, None, True)
+    if res is s and not isinstance(res, bytes):
+        res = bytes(res)
+    return res
 
 # Extension
 def locsplit_to_tuple(netloc, *, missing_as_none=False):
+    if netloc is None:
+        if missing_as_none:
+            return (None, None, None, None)
+        raise TypeError("netloc must be str or bytes")
+
     ss = isinstance(netloc, str)
+    _check_authority(netloc, ss)
     missing = None if missing_as_none else "" if ss else b""
 
     if (sep := netloc.rfind('@' if ss else b'@')) >= 0:
@@ -303,7 +317,7 @@ def locsplit_to_tuple(netloc, *, missing_as_none=False):
             host, port = hostport[sep+1:port], hostport[port+1:]
         else:
             raise ValueError("bad IPv6 address")
-    elif (sep := hostport.rfind(':' if ss else b':')) >= 0:
+    elif (sep := hostport.find(':' if ss else b':')) >= 0:
         host, port = hostport[:sep], hostport[sep:]
     else:
         host, port = hostport, missing
@@ -332,55 +346,102 @@ def locsplit_to_tuple(netloc, *, missing_as_none=False):
 def locsplit(netloc, *, missing_as_none=False):
     return dict(zip(('username', 'password', 'hostname', 'port'), locsplit_to_tuple(netloc, missing_as_none=missing_as_none)))
 
+@micropython.viper
+def _is_scheme(buf_ptr: ptr8, start: int, colon: int) -> bool:
+    if start >= colon:
+        return False
+    x = buf_ptr[start]
+    if x < 65 or (x > 90 and x < 97) or x > 122:
+        return False
+    start += 1
+    while start < colon:
+        x = buf_ptr[start]
+        start += 1
+        if (not (48 <= x and x <= 57)
+            and not (65 <= x and x <= 90)
+            and not (97 <= x and x <= 122)
+            and x != 43
+            and x != 45
+            and x != 46
+        ):
+            return False
+    return True
+
+def _check_authority(netloc, ss):
+    if not netloc:
+        return
+    at = netloc.rfind('@' if ss else b'@') + 1
+    lb = netloc.find('[' if ss else b'[', at)
+    rb = netloc.find(']' if ss else b']', at)
+    if ((lb < 0) ^ (rb < 0) or (
+            lb >= 0 and (
+                lb != at or
+                rb <= lb + 1 or
+                (rb + 1 < len(netloc) and
+                 not netloc.startswith(':' if ss else b':', rb + 1))
+            ))):
+        raise ValueError("bad IPv6 authority")
+
 def urlsplit_to_tuple(url, scheme=None, allow_fragments=True, *, missing_as_none=False):
     if url is None:
-        url = ""
-    if scheme and (isinstance(url, str) ^ isinstance(scheme, str)):
-        raise TypeError("arguments must be similar types")
+        url = b"" if isinstance(scheme, (bytes, bytearray)) else ""
     ss = isinstance(url, str)
     missing = None if missing_as_none else "" if ss else b""
+    if scheme is None:
+        scheme = missing
+    elif isinstance(url, str) ^ isinstance(scheme, str):
+        if scheme:
+            raise TypeError("arguments must be similar types")
+        scheme = missing
+    elif isinstance(scheme, bytearray):
+        scheme = scheme[:]
 
-    starts = 0
-    finish = len(url)
+    start = 0
+    end = len(url)
+
+    while start < end:
+        if url[start] > (" " if ss else 32):
+            break
+        start += 1
 
     netloc = query = frag = missing
     if scheme is None:
         scheme = missing
 
-    if allow_fragments and (i := url.find('#' if ss else b'#', starts)) >= 0:
+    if allow_fragments and (i := url.find('#' if ss else b'#', start)) >= 0:
         frag = url[i+1:]
-        finish = i
+        end = i
 
-    if (i := url.find('?' if ss else b'?', starts)) >= 0 and i < finish:
-        query = url[i+1:finish]
-        finish = i
+    if (i := url.find('?' if ss else b'?', start)) >= 0 and i < end:
+        query = url[i+1:end]
+        end = i
 
-    colon = url.find(':' if ss else b':', starts)
-    if starts < colon < finish and url[starts:starts+1].isalpha():
-        slash = url.find('/' if ss else b'/', starts)
+    colon = url.find(':' if ss else b':', start)
+    if start < colon < end and _is_scheme(url, start, colon):
+        slash = url.find('/' if ss else b'/', start)
         if slash < 0 or colon < slash:
-            scheme = url[starts:colon].lower()
-            starts = colon + 1
+            scheme = url[start:colon].lower()
+            start = colon + 1
 
-    if url.startswith("//" if ss else b"//", starts):
-        starts += 2
-        slash = url.find('/' if ss else b'/', starts)
-        delim = slash if (0 <= slash < finish) else finish
-        netloc = url[starts:delim]
-        starts = delim
+    if url.startswith("//" if ss else b"//", start):
+        start += 2
+        slash = url.find('/' if ss else b'/', start)
+        delim = slash if (0 <= slash < end) else end
+        netloc = url[start:delim]
+        start = delim
 
-    if netloc and (i := netloc.find('[' if ss else b'[')) >= 0:
-        if not (netloc.rfind('@' if ss else b'@') + 1 == i < netloc.find(']' if ss else b']')):
-            raise ValueError("bad IPv6 address")
+    _check_authority(netloc, ss)
 
-    return (scheme, netloc, url[starts:finish], query, frag)
+    return (scheme, netloc, url[start:end], query, frag)
 
 class SplitResult(tuple):
 
     def __init__(self, scheme, netloc, path, query, frag):
         super().__init__((scheme, netloc, path, query, frag))
-        self._keep_empty = False
         self._locsplit_ = None
+        if not isinstance(netloc, (str, bytes)):
+            self._locsplit
+        self._keep_empty = False
 
     @property
     def _locsplit(self):
@@ -424,10 +485,8 @@ class SplitResult(tuple):
     def geturl(self):
         return urlunsplit(self)
 
-def urlsplit(url, scheme=None, allow_fragments=True, *, missing_as_none=False):
+keep_def urlsplit(url, scheme=None, allow_fragments=True, *, missing_as_none=False):
     res = SplitResult(*urlsplit_to_tuple(url, scheme, allow_fragments, missing_as_none=missing_as_none))
-    if missing_as_none:
-        res._keep_empty = True
     return res
 
 def _urlunsplit(scheme, netloc, path, query, frag):
@@ -491,22 +550,23 @@ def urlunsplit(components, *, keep_empty=None):
     return _urlunsplit(scheme, netloc, path, query, frag)
 
 def _urlencode_generator(query, doseq, safe, quote_via, equals):
-    bytes_like = (str, bytes, bytearray)
-
     if hasattr(query, "items"):
         query = query.items()
-    for key, val in query:
-        if not isinstance(key, bytes_like):
+    for item in query:
+        if not isinstance(item, (list, tuple)) or len(item) != 2:
+            raise TypeError("query item is not a key-value pair")
+        key, val = item
+        if not isinstance(key, (str, bytes, bytearray)):
             key = str(key)
         key = quote_via(key, safe)
 
-        if not isinstance(val, bytes_like):
+        if not isinstance(val, (str, bytes, bytearray)):
             if doseq and not isinstance(val, memoryview):
                 try: len(val)
                 except TypeError: pass
                 else:
                     for v in val:
-                        if not isinstance(v, bytes_like):
+                        if not isinstance(v, (str, bytes, bytearray)):
                             v = str(v)
                         yield key + equals + quote_via(v, safe)
                     continue
@@ -514,16 +574,19 @@ def _urlencode_generator(query, doseq, safe, quote_via, equals):
         yield key + equals + quote_via(val, safe)
 
 def urlencode(query, doseq=False, safe="", quote_via=quote_plus):
-    if safe and not isinstance(safe, _compiled_blob):
-        if quote_via is quote_plus:
-            safe = compile_safe(safe, 1)
-        elif quote_via is quote:
-            safe = compile_safe(safe, 0)
-
-    if quote_via is quote_plus or isinstance(quote_via(""), str):
+    if quote_via is quote_plus or quote_via is quote:
+        if safe and not isinstance(safe, _compiled_safe):
+            safe = compile_safe(safe, 1 if quote_via is quote_plus else 0)
+        separator, equals = "&", "="
+    elif quote_via is quote_plus_to_bytes or quote_via is quote_to_bytes:
+        if safe and not isinstance(safe, _compiled_safe):
+            safe = compile_safe(safe, 1 if quote_via is quote_plus_to_bytes else 0)
+        separator, equals = b"&", b"="
+    elif isinstance(quote_via(""), str):
         separator, equals = "&", "="
     else:
         separator, equals = b"&", b"="
+
     return separator.join(
         _urlencode_generator(
             query,
@@ -535,7 +598,7 @@ def urlencode(query, doseq=False, safe="", quote_via=quote_plus):
     )
 
 @micropython.viper
-def _find_equalsep(mv: ptr8, start: int, end: int, separator: int) -> object:
+def _parse_helper(mv: ptr8, start: int, end: int, separator: int) -> object:
     equals = -1
     i = start
     while i < end:
@@ -552,7 +615,6 @@ def _parse_generator(s, *, keep_blank_values=False, strict_parsing=False,
     if s is None:
         return
     if isinstance(s, str):
-        # on micropython, memoryview(str) gives you direct access to the underlying bytes
         src = memoryview(s)
         do_decode = True
     else:
@@ -563,7 +625,9 @@ def _parse_generator(s, *, keep_blank_values=False, strict_parsing=False,
         return
 
     try: sep = ord(separator)
-    except TypeError: raise ValueError("invalid separator")
+    except TypeError: sep = -1
+    if not (0 <= sep <= (127 if do_decode else 255)):
+        raise ValueError("invalid separator")
     i = 0
     num_fields = 0
 
@@ -572,30 +636,30 @@ def _parse_generator(s, *, keep_blank_values=False, strict_parsing=False,
             num_fields += 1
             if num_fields > max_num_fields:
                 raise ValueError("Max number of fields exceeded")
-        eq, j = _find_equalsep(src, i, srclen, sep)
+        eq, j = _parse_helper(src, i, srclen, sep)
         if i == j:
             if strict_parsing:
                 raise ValueError("bad query field")
             i = j + 1
             continue
 
+        if eq < 0:
+            if strict_parsing:
+                raise ValueError("bad query field")
+            if not keep_blank_values:
+                i = j + 1
+                continue
+            eq = j
+            val = b""
+        else:
+            if not keep_blank_values and eq + 1 == j:
+                i = j + 1
+                continue
+            val = _unquote(src, eq + 1, j, True)
+
+        key = _unquote(src, i, eq, True)
+
         try:
-            if eq < 0:
-                if strict_parsing:
-                    raise ValueError("bad query field")
-                if not keep_blank_values:
-                    i = j + 1
-                    continue
-                eq = j
-                val = b""
-            else:
-                if not keep_blank_values and eq + 1 == j:
-                    i = j + 1
-                    continue
-                val = _unquote(src, eq + 1, j, True)
-
-            key = _unquote(src, i, eq, True)
-
             if do_decode:
                 key = key.decode()
                 val = val.decode()
