4452ea9937dfe603835422b3697b337601b6cafa
robtinkers <74528586+robtinkers@users.noreply.github.com>
2026-08-21 12:43:41 +0100

    Update client_ish.py

--- client_ish.py
+++ client_ish.py
@@ -11,7 +11,7 @@ _COMPATISH_DECODE_HEADERS = const(0)
 
 _EXTRA_METHODS = const(1)
 _ITERATE_HEADERS = const(1)
-_PARSE_REASON = const(1)
+_PARSE_REASON = const(0)
 _RECYCLE_BUFFERS = const(1)
 _SSL_ENABLED = const(1)
 
